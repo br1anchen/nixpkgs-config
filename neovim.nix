85eef0be4c6a7a9a8f839986ca4f6ca5401a76cd
Brian Chen <brianchen8990@gmail.com>
2022-08-28 14:02:28 +0200

    Remove pip packages

--- neovim.nix
+++ neovim.nix
@@ -16,18 +16,6 @@
     sqlfluff
     jupyter
     black
-    python39Packages.pyqt5
-    python39Packages.qtconsole
-    python39Packages.jupytext
-    python39Packages.ipython
-    python39Packages.ipykernel
-    python39Packages.matplotlib-inline
-    python39Packages.matplotlib
-    python39Packages.numpy
-    python39Packages.pandas
-    python39Packages.scipy
-    python39Packages.scikit-learn
-    python39Packages.joblib
   ];
 
   xdg.configFile.nvim = {
