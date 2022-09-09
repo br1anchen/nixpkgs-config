7eab27428286a0f3ff3188620b76fab6adcbf714
Brian Chen <brianchen8990@gmail.com>
2022-09-09 07:37:56 +0200

    Add latex deps

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -14,8 +14,14 @@
     statix
     code-minimap
     sqlfluff
-    jupyter
     black
+    zathura
+    python39Packages.pynvim
+    python39Packages.jupyter
+    python39Packages.ueberzug
+    python39Packages.pillow
+    python39Packages.cairosvg
+    python39Packages.plotly
   ];
 
   xdg.configFile.nvim = {
