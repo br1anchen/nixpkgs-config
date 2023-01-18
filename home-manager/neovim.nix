bd8faf5fbc03aba2a144ed65e7b69deaae4bf55a
Brian Chen <brianchen8990@gmail.com>
2023-01-18 10:04:54 +0100

    refactor(home-manager/neovim): move broken python310 packages to linux only

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -5,18 +5,16 @@
 {
 
   home.packages = with pkgs;
-    [
-      luajitPackages.luarocks
-      nixfmt
-      statix
-      code-minimap
+    [ luajitPackages.luarocks nixfmt statix code-minimap ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
       jupyter
       zathura
       python39Packages.pynvim
       python39Packages.ueberzug
       python39Packages.pillow
       python39Packages.plotly
-    ] ++ lib.optionals pkgs.stdenv.isLinux [ python39Packages.cairosvg ];
+      python39Packages.cairosvg
+    ];
 
   xdg.configFile.nvim = {
     source = ~/nixpkgs-config/config/neovim;
