a33c46e7f2f3f07c9c1dcfce0783712c9f93d4ad
Brian Chen <brianchen8990@gmail.com>
2023-02-16 10:47:43 +0100

    refactor(config/wezterm): update font config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,14 +1,8 @@
 local wezterm = require("wezterm")
-local mux = wezterm.mux
-
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
 
 return {
 	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font("scientifica"),
-	font_size = 13.5,
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 13,
 	window_decorations = "RESIZE",
 }
