2ef2e09918ac214f70dc3c537ed1d95b0c8540d5
Brian Chen <brianchen8990@gmail.com>
2024-12-05 21:51:20 +0100

    fix(config/asdf): after install asdf shell is not reload

--- config/asdf/setup.sh
+++ config/asdf/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.asdf" ]; then
-	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+  source ~/.zshrc
 fi
 
 # Define the fixed filename
@@ -9,18 +10,18 @@ filename="$HOME/nixpkgs-config/config/asdf/tool-versions"
 
 # Check if the file exists
 if [ ! -f "$filename" ]; then
-	echo "File not found: $filename"
-	exit 1
+  echo "File not found: $filename"
+  exit 1
 fi
 
 # Read the file line by line
 while IFS= read -r line; do
-	# Extract the first entry of the line
-	first_entry=$(echo "$line" | awk '{print $1}')
+  # Extract the first entry of the line
+  first_entry=$(echo "$line" | awk '{print $1}')
 
-	echo "Adding plugin: $first_entry"
-	# Use the first entry in the command "asdf plugin add"
-	asdf plugin add "$first_entry"
+  echo "Adding plugin: $first_entry"
+  # Use the first entry in the command "asdf plugin add"
+  asdf plugin add "$first_entry"
 done <"$filename"
 
 asdf install
