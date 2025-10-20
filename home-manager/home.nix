e23ac870c005977db2b5f14441a741b9e8cba0ae
Brian Chen <brianchen8990@gmail.com>
2025-10-21 00:02:58 +0200

    fix(home-manager/home): missing tag,gpg dep

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,54 +53,52 @@
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  home.packages =
-    with pkgs;
-    [
-      bash
-      cachix # Nix build cache
-      cheat
-      curl # An old classic
-      colima
-      cocogitto
-      docker # World's #1 container tool
-      docker-compose
-      entr
-      eza # ls replacement written in Rust
-      fd # find replacement written in Rust
-      fswatch
-      gitui
-      glow
-      gnumake
-      go
-      getopt
-      htop # Resource monitoring
-      # httpie # Like curl but more user friendly
-      jq # JSON parsing for the CLI
-      kubectl
-      mcfly
-      mdcat # Markdown converter/reader for the CLI
-      mkcert
-      minikube
-      nerd-font-patcher
-      neofetch
-      nix-prefetch-github
-      ncdu
-      pandoc
-      procs
-      protobuf
-      ripgrep # grep replacement written in Rust
-      rustup
-      scientifica
-      wget
-      xclip
-      xh
-      zoxide
-      _1password-cli
-      _1password-gui
-      nerd-fonts.fira-code
-      watchexec
-    ]
-    ++ lib.optionals pkgs.stdenv.isLinux [
-      starship # Fancy shell that works with zsh
-    ];
+  home.packages = with pkgs; [
+    bash
+    cachix # Nix build cache
+    cheat
+    curl # An old classic
+    colima
+    cocogitto
+    docker # World's #1 container tool
+    docker-compose
+    entr
+    eza # ls replacement written in Rust
+    fd # find replacement written in Rust
+    fswatch
+    gitui
+    glow
+    gnumake
+    gnutar
+    gnupg
+    go
+    getopt
+    htop # Resource monitoring
+    # httpie # Like curl but more user friendly
+    jq # JSON parsing for the CLI
+    kubectl
+    mcfly
+    mdcat # Markdown converter/reader for the CLI
+    mkcert
+    minikube
+    nerd-font-patcher
+    neofetch
+    nix-prefetch-github
+    ncdu
+    pandoc
+    procs
+    protobuf
+    ripgrep # grep replacement written in Rust
+    rustup
+    scientifica
+    wget
+    xclip
+    xh
+    zoxide
+    _1password-cli
+    _1password-gui
+    nerd-fonts.fira-code
+    watchexec
+    starship # Fancy shell that works with zsh
+  ];
 }
